c3ebc6733b8fac8f59bb172a8cf37609558e1549
terrylin <terry.lin@musical.ly>
2016-04-11 14:37:16 +0800

    Initial Commit

---
+++ PodsTrending/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodsTrending
+//
+//  Created by terrylin on 16/4/11.
+//  Copyright © 2016年 TerryLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodsTrending/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodsTrending
+//
+//  Created by terrylin on 16/4/11.
+//  Copyright © 2016年 TerryLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
